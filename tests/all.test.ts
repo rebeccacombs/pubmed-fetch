1bd7bd5a4e04ab3e316ef8a178c3ba431ab7c40f
rebeccacombs <rebecca.brushes@gmail.com>
2024-10-18 14:07:37 +0800

    messing w/ tests

--- tests/all.test.ts
+++ tests/all.test.ts
@@ -1,13 +1,12 @@
-import { buildQuery, } from '../src';
+import getIDsAndData, { buildQuery, } from '../src';
 const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS)
 
 //const api_key = process.env.NCBI_API_KEY
-//const query = buildQuery(authors, topics, dateRange)
 const authors = ['']
 const topics = ['RNAi', "siRNA", "ASO", "mRNA"]
 const dateRange = '("2024/09/19"[Date - Create] : "2024/10/15"[Date - Create])'
-
+//const query = buildQuery(authors, topics, dateRange)
 
 test('builds a query', () => {
   const result = buildQuery(authors, topics, dateRange);
@@ -19,4 +18,4 @@ test('getting all', async () => {
   const ret = await getIDsAndData(query, 2, api_key, true)
   expect(ret).toEqual("returnData")
 });
-*/ 
+*/
